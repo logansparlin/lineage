a4637da429bdd257cb569505bdf6e7cf54951875
Logan Sparlin <logan.sparlin@gmail.com>
2025-03-20 03:55:27 -0400

    Update scroll-config.ts

--- lib/scroll-config.ts
+++ lib/scroll-config.ts
@@ -1,9 +1,9 @@
 export const scrollConfig = {
   anchors: true,
   syncTouch: true,
-  touchMultiplier: 0.8,
-  syncTouchLerp: 0.08,
-  touchInertiaMultiplier: 25,
+  touchMultiplier: 0.75,
+  syncTouchLerp: 0.07,
+  touchInertiaMultiplier: 20,
   lerp: 0.09,
   wheelMultiplier: 0.8,
 }
