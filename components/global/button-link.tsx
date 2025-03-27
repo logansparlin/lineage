2a210770343a2bac0b07b96d545be4f5aea99e92
Logan Sparlin <logan.sparlin@gmail.com>
2025-03-27 11:44:10 -0400

    Start time

--- components/global/button-link.tsx
+++ components/global/button-link.tsx
@@ -8,7 +8,7 @@ export const ButtonLink: FC<ButtonLinkProps> = ({ children, className,  ...props
     <Link
       {...props}
       scroll={false}
-      className={`${className} px-12 md:px-14 h-30 md:h-36 text-20 !leading-100 border-1 border-white rounded-full flex items-center justify-center hover:bg-white hover:text-black transition-colors duration-300`}
+      className={`${className} px-12 md:px-14 h-30 md:h-36 text-20 !leading-100 border-1 border-white rounded-full flex items-center justify-center all-interactions:bg-white all-interactions:text-black transition-colors duration-300`}
     >
       {children}
     </Link>
